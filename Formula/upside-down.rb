77b4563af7e41d140406f66e10ff9cac5b352408
Marcus Smith <marcus.smith@stablekernel.com>
2019-05-03 14:04:42 -0400

    Fix formula URL

--- Formula/upside-down.rb
+++ Formula/upside-down.rb
@@ -1,17 +1,14 @@
 class UpsideDown < Formula
   desc "command line interface for upside down"
   homepage "https://github.com/upside-down-swift/upside-down"
-  url "https://github.com/upside-down-swift/cli", :revision => "c160d6de83c3cfab770bb21e799d56b64e1f7838"
+  url "https://github.com/upside-down-swift/cli.git", :tag => "0.1.0", :revision => "c160d6de83c3cfab770bb21e799d56b64e1f7838"
+  head "https://github.com/upside-down-swift/cli.git", :shallow => false
   version "0.1.0"
-  
+
   depends_on :xcode => ["10.0", :build]
   depends_on "buildkit" => "0.5.0"
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
     system "make", "install", "prefix=#{prefix}"
   end
 
